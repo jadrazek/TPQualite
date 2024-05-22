173fcbe3e169419be43987f98135d25075f57e4f
LOSAT Giovanni <giolstpro@gmail.com>
2024-05-22 12:30:35 +0200

    Modif plateau etudiant n°3

--- correction_projet_echec/Plateau.java
+++ correction_projet_echec/Plateau.java
@@ -3,31 +3,61 @@ import java.util.ArrayList;
 class Plateau{
     private ArrayList<Piece> pieces;
 
-    public Plateau(){
-	pieces = new ArrayList<Piece>();
-	this.ajouterPiece(new Tour('B', new Position("A1")));
-	this.ajouterPiece(new Cavalier('B', new Position("B1")));
-	this.ajouterPiece(new Fou('B', new Position("C1")));
-	this.ajouterPiece(new Dame('B', new Position("D1")));
-	this.ajouterPiece(new Roi('B', new Position("E1")));
-	this.ajouterPiece(new Fou('B', new Position("F1")));
-	this.ajouterPiece(new Cavalier('B', new Position("G1")));
-	this.ajouterPiece(new Tour('B', new Position("H1")));
+    public Plateau (boolean cheat)
+	{
+		pieces = new ArrayList<Piece>();
 
-	for(char c = 'A'; c < 'I' ; c++){
-	    this.ajouterPiece(new PionBlanc(new Position(c+"2")));
-	    this.ajouterPiece(new PionNoir(new Position(c+"7")));
-	    }
+		if (cheat != true) // si c'est faux , ajout des pièces classiques
+		{
+			this.ajouterPiece(new Tour('B', new Position("A1")));
+			this.ajouterPiece(new Cavalier('B', new Position("B1")));
+			this.ajouterPiece(new Fou('B', new Position("C1")));
+			this.ajouterPiece(new Dame('B', new Position("D1")));
+			this.ajouterPiece(new Roi('B', new Position("E1")));
+			this.ajouterPiece(new Fou('B', new Position("F1")));
+			this.ajouterPiece(new Cavalier('B', new Position("G1")));
+			this.ajouterPiece(new Tour('B', new Position("H1")));
 
-	this.ajouterPiece(new Tour('N', new Position("A8")));
-	this.ajouterPiece(new Cavalier('N', new Position("B8")));
-	this.ajouterPiece(new Fou('N', new Position("C8")));
-	this.ajouterPiece(new Dame('N', new Position("D8")));
-	this.ajouterPiece(new Roi('N', new Position("E8")));
-	this.ajouterPiece(new Fou('N', new Position("F8")));
-	this.ajouterPiece(new Cavalier('N', new Position("G8")));
-	this.ajouterPiece(new Tour('N', new Position("H8")));
-    }
+			for(char c = 'A'; c < 'I' ; c++){
+				this.ajouterPiece(new PionBlanc(new Position(c+"2")));
+				this.ajouterPiece(new PionNoir(new Position(c+"7")));
+				}
+
+			this.ajouterPiece(new Tour('N', new Position("A8")));
+			this.ajouterPiece(new Cavalier('N', new Position("B8")));
+			this.ajouterPiece(new Fou('N', new Position("C8")));
+			this.ajouterPiece(new Dame('N', new Position("D8")));
+			this.ajouterPiece(new Roi('N', new Position("E8")));
+			this.ajouterPiece(new Fou('N', new Position("F8")));
+			this.ajouterPiece(new Cavalier('N', new Position("G8")));
+			this.ajouterPiece(new Tour('N', new Position("H8")));
+		}
+		else // sinon ajout des tavaliers et favaliers .
+		{
+			this.ajouterPiece(new Tour('B', new Position("A1")));
+			this.ajouterPiece(new Cavalier('B', new Position("B1")));
+			this.ajouterPiece(new Tavalier('B', new Position("C1")));
+			this.ajouterPiece(new Favalier('B', new Position("D1")));
+			this.ajouterPiece(new Favalier('B', new Position("E1")));
+			this.ajouterPiece(new Tavalier('B', new Position("F1")));
+			this.ajouterPiece(new Cavalier('B', new Position("G1")));
+			this.ajouterPiece(new Tour('B', new Position("H1")));
+
+			for(char c = 'A'; c < 'I' ; c++){
+				this.ajouterPiece(new PionBlanc(new Position(c+"2")));
+				this.ajouterPiece(new PionNoir(new Position(c+"7")));
+				}
+
+			this.ajouterPiece(new Tour('N', new Position("A8")));
+			this.ajouterPiece(new Cavalier('N', new Position("B8")));
+			this.ajouterPiece(new Tavalier('N', new Position("C8")));
+			this.ajouterPiece(new Favalier('N', new Position("D8")));
+			this.ajouterPiece(new Favalier('N', new Position("E8")));
+			this.ajouterPiece(new Tavalier('N', new Position("F8")));
+			this.ajouterPiece(new Cavalier('N', new Position("G8")));
+			this.ajouterPiece(new Tour('N', new Position("H8")));
+		}
+	}
 
 
     private boolean ajouterPiece(Piece p){
